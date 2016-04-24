2dfc612406258106d7f1e2a6004c14c8c6a54d00
Fumarola Fabio (UniCredit) <fabio.fumarola@unicredit.eu>
2016-04-24 11:41:32 +0200

    refactoring

--- src/main/scala/it/dtk/reactive/jobs/ProcessFeeds.scala
+++ src/main/scala/it/dtk/reactive/jobs/ProcessFeeds.scala
@@ -4,8 +4,6 @@ import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream.scaladsl._
 import akka.stream.{ActorMaterializer, ClosedShape, ThrottleMode}
-import com.sksamuel.elastic4s.streams.ReactiveElastic._
-import com.sksamuel.elastic4s.streams.ScrollPublisher
 import com.typesafe.config.ConfigFactory
 import it.dtk.es._
 import it.dtk.model.{Feed, SchedulerData}
@@ -13,18 +11,14 @@ import it.dtk.protobuf._
 import it.dtk.reactive.jobs.helpers._
 import net.ceedubs.ficus.Ficus._
 import org.joda.time.DateTime
-import org.json4s.NoTypeHints
-import org.json4s.ext.JodaTimeSerializers
-import org.json4s.jackson.JsonMethods._
-import org.json4s.jackson.Serialization
 import redis.clients.jedis.Jedis
 
 import scala.concurrent.duration._
 import scala.language.implicitConversions
 
 /**
-  * Created by fabiofumarola on 09/03/16.
-  */
+ * Created by fabiofumarola on 09/03/16.
+ */
 class ProcessFeeds(configFile: String)(implicit val system: ActorSystem,
                                        implicit val mat: ActorMaterializer) {
   val config = ConfigFactory.load(configFile).getConfig("reactive_wtl")
@@ -96,7 +90,6 @@ class ProcessFeeds(configFile: String)(implicit val system: ActorSystem,
       .throttle(1, 1.second, 200, ThrottleMode.Shaping)
       .map(gander.mainContent)
       .map(a => kafka.wrap(a))
-
 
   def extractArticles(): Flow[Feed, (Feed, List[Article]), NotUsed] =
     Flow[Feed].map { f =>
